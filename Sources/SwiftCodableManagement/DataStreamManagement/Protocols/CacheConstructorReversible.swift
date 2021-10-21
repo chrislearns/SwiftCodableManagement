363042975d15bf4cee002b2aab9fa257e99b673d
Chris Guirguis <christopher.g.codes@gmail.com>
2021-10-20 23:08:24 -0400

    changes to cacheconstructorreversible

--- Sources/SwiftCodableManagement/DataStreamManagement/Protocols/CacheConstructorReversible.swift
+++ Sources/SwiftCodableManagement/DataStreamManagement/Protocols/CacheConstructorReversible.swift
@@ -9,14 +9,12 @@ import SwiftUI
 
 //This protocol requires the presence of a CacheNameConstructor which allows for easy generation of the constructed cache name via a computed variable
 public protocol CacheConstructorReversible:Codable {
-    var id: UUID { get }
-    
     static var cachePrefix: String { get }
     static var cacheSuffix: String { get }
 }
 
 extension CacheConstructorReversible {
-    public var cacheNameConstructor:CacheNameConstructor {
-        .init(prefix: Self.cachePrefix, suffix: Self.cacheSuffix, uniqueIdentifier: id.uuidString)
+    public func cacheNameConstructor(_ id: UUID? = nil) -> CacheNameConstructor {
+        .init(prefix: Self.cachePrefix, suffix: Self.cacheSuffix, uniqueIdentifier: id?.uuidString)
     }
 }
