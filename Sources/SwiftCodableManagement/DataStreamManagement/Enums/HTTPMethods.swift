ccf062ff251099966ac8e5ed69a762de7a7daafa
chris <christopher.g.codes@gmail.com>
2021-11-22 09:37:34 -0500

    Update HTTPMethods.swift

--- Sources/SwiftCodableManagement/DataStreamManagement/Enums/HTTPMethods.swift
+++ Sources/SwiftCodableManagement/DataStreamManagement/Enums/HTTPMethods.swift
@@ -11,13 +11,13 @@ import Alamofire
 public enum SCMHTTPMethod: String {
     case get = "GET"
     case post = "POST"
+    case delete = "DELETE"
     
     public var httpMethod: HTTPMethod {
         switch self {
-        case .get:
-            return .get
-        case .post:
-            return .post
+        case .get: return .get
+        case .post: return .post
+        case .delete: return .delete
         }
     }
 }
